2228a33c60585ea84ac7eea78f180046c20df8ab
joel-gomez-ch <jgomezc@unjbg.edu.pe>
2026-06-18 20:01:00 -0500

    Actualizacion de AReverso: Agrege lineas de salida para tener una vista mas agradable al ejecutar

--- AReverso.cpp
+++ AReverso.cpp
@@ -3,15 +3,18 @@ using namespace std;
 
 int main() {
     int N;
-    
+
+    cout << "Indique la cantidad de elementos que tendra el arreglo: ";
     cin >> N;
     if(1<=N && N<= 1000){
         int numeros[N];
     
         for (int i = 0; i < N; i++) {
+            cout << "Ingrese el elemento " << i+1 <<": ";
             cin >> numeros[i];
         }
 
+        cout << "Los elementos en orden inverso serian: ";
         for (int i = N - 1; i >= 0; i--) {
             cout << numeros[i] << " ";
         }
