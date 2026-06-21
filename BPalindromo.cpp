39fddfa7ee7ff3f05ff4a70b8ab5d8c3fbc3b271
joel-gomez-ch <jgomezc@unjbg.edu.pe>
2026-06-21 18:27:37 -0500

    Actualizacion de BPalindromo (Agregue comentarios para entender el codigo)

--- BPalindromo.cpp
+++ BPalindromo.cpp
@@ -6,11 +6,13 @@ using namespace std;
 int main() {
 
     int N;
+    // Leer la cantidad de palabras a evaluar
     cout << "Ingrese la cantidad de palabras: ";
     cin >> N;
 
     string resultado[N];
 
+    // Lee cada palabra y verificar si es palíndromo
     for (int a = 0; a < N; a++) {
 
         string palabra;
@@ -20,6 +22,7 @@ int main() {
         bool palindromo = true;
         int longitud = palabra.length();
 
+        // Compara cada caracter desde los extremos hacia el centro
         for (int i = 0; i < longitud / 2; i++) {
 
             if (palabra[i] != palabra[longitud - 1 - i]) {
@@ -28,13 +31,15 @@ int main() {
             }
         }
 
+        // Guardar el resultado
         if (palindromo){
             resultado[a] = "P";
         }else {
             resultado[a] = "NP";
         }
     }
-    
+
+    // Muestra los resultados obtenidos
     cout << "\nResultados:\n";
     for (int k = 0; k < N; k++) {
         cout << resultado[k] << endl;
