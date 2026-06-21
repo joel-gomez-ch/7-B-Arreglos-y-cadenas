56b9ce44323ad9f13a295479d02ab49612391479
joel-gomez-ch <jgomezc@unjbg.edu.pe>
2026-06-21 18:39:04 -0500

    Actualizacion de CFrecuencias (Agregue salidas para un entorno mas amigable al usuario)

--- CFrecuencias.cpp
+++ CFrecuencias.cpp
@@ -5,18 +5,24 @@ using namespace std;
 int main() {
 
     int P, n;
-    cin >> P >> n;
+    cout << "Ingrese la cantidad de profesores: ";
+    cin >> P;
+    cout << "Ingrese la cantidad de alumnos: ";
+    cin >> n;
 
     int frecuencia[P] = {0};
 
+    cout << "\nIngrese el profesor favorito de cada alumno:\n";
     for (int i = 0; i < n; i++) {
 
         int F;
+        cout << "Alumno " << i + 1 << ": ";
         cin >> F;
 
         frecuencia[F - 1]++;
     }
 
+    cout << "\nTabla de resultados:\n";
     for (int i = 0; i < P; i++) {
         cout << i + 1 << "-" << frecuencia[i] << endl;
     }
