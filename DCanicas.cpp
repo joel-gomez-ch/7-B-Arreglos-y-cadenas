d498a9569f23cebf3e91aafc31efbb2b8bd57e98
joel-gomez-ch <jgomezc@unjbg.edu.pe>
2026-06-23 17:33:28 -0500

    Ejercicio DCanicas (lectura de las N canicas lanzadas)

--- DCanicas.cpp
+++ DCanicas.cpp
@@ -7,5 +7,13 @@ int main() {
     cin >> N >> C;
 
     int vasos[N] = {0};
+    
+    for (int i = 0; i < C; i++) {
+
+        int vaso;
+        cin >> vaso;
+
+        vasos[vaso - 1]++;
+    }
     return 0;
 }
