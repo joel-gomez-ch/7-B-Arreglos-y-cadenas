b08ec759c0a4ce966fd2f72549b28e02032f4013
joel-gomez-ch <jgomezc@unjbg.edu.pe>
2026-06-23 17:39:00 -0500

    Actualizacion de DCanicas (Se gregaron salidas para una vista mas agradable)

--- DCanicas.cpp
+++ DCanicas.cpp
@@ -4,20 +4,32 @@ using namespace std;
 int main() {
 
     int N, C;
-    cin >> N >> C;
+
+    cout << "Ingrese la cantidad de vasos: ";
+    cin >> N;
+
+    cout << "Ingrese la cantidad de canicas lanzadas: ";
+    cin >> C;
 
     int vasos[N] = {0};
+
+    cout << "\nIngrese el numero del vaso donde cayo cada canica:\n";
 
     for (int i = 0; i < C; i++) {
 
         int vaso;
+
+        cout << "Canica " << i + 1 << ": ";
         cin >> vaso;
 
         vasos[vaso - 1]++;
     }
 
+    cout << "\nCantidad de canicas en cada vaso:\n";
+
     for (int i = 0; i < N; i++) {
-        cout << vasos[i] << endl;
+        cout << "Vaso " << i + 1 << ": "
+             << vasos[i] << " canicas" << endl;
     }
 
     return 0;
